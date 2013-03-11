7103300a1c1eda0aaad17d1dd7a3458bd1380260
Shan Carter <shanjcarter@gmail.com>
2013-03-11 10:36:16 -0700

    refactoring as a bookmarklet

--- svg-crowbar.js
+++ svg-crowbar.js
@@ -1,68 +1,153 @@
-var SVGCrowbar = {
+(function() {
+  var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
 
-  init: function(filename, selector) {
+  var styles = getStyles();
 
-    var title = filename ? filename : "untitled",
-        svg = d3.select(selector ? selector : "svg"),
-        parent = d3.select("body"),
-        doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+  var SVGSources = getSources(styles);
 
-    var link = parent.append("a")
-        .attr("href", "#")
-        .attr("class", "svg-crowbar")
-        .attr("target", "_blank")
-        .attr("download", title + ".svg")
-        .style("width", "250px")
-        .style("font-size", "16px")
-        .style("display", "inline-block")
-        .style("text-align", "center")
-        .style("border-radius", "3px")
-        .style("padding", "6px")
+  createPopover(SVGSources);
+
+  //
+  //
+  //
+  function createPopover(sources) {
+    var body = d3.select("body");
+
+    var html = body.append("div")
+        .style("position", "fixed")
+        .style("top", 0)
+        .style("right", 0)
+        .style("background", "rgba(0, 0, 0, 0.8)")
+        .style("padding", "10px")
+        .style("margin", "10px")
+        .style("border-radius", "4px")
         .style("color", "white")
-        .style("background", "#004276")
-        .style("margin", "10px 10px 0 0")
-        .text("Download " + title + ".svg")
+        .style("font-family", "'Helvetica Neue'");
 
-    link.on("mouseover", function() {
-      var styles = "",
-          styleSheets = window.document.styleSheets,
-          link = d3.select(this);
+    var headline = html.append("div")
+        .text("The Crowbar");
 
+    headline.append("span")
+        .text(" found " + (sources.length === 0 ? "no" : sources.length) + " SVG node" + (sources.length === 1 ? "" : "s"));
+
+    var buttons = html.append("div")
+        .attr("class", "buttons");
+
+    var button = buttons.selectAll(".button")
+        .data(sources)
+      .enter().append("div")
+        .attr("class", "button")
+        .style("font-size", "13px")
+
+    button.append("div")
+        .attr("class", "download")
+        .text(function(d, i) { return "Download #" + (i + 1); })
+        .style("cursor", "pointer")
+        .style("margin", "10px 0 10px 0")
+        .style("font-size", "16px")
+        .style("padding", "4px")
+        .style("border-radius", "3px")
+        .style("border", "solid 1px rgba(255, 255, 255, 0.8)")
+        .style("background", "black")
+        .on("click", function(d, i) {
+          d3.event.preventDefault();
+          download(d.source)
+        });
+
+    button.each(function(source) {
+      var detail = d3.select(this).selectAll(".detail")
+          .data(["id", "class", "childElementCount", "top", "left", "width", "height"])
+        .enter().append("div")
+          .attr("class", "detail");
+
+      detail.append("span")
+          .style("color", "#bbb")
+          .text(function(d) { return d + ": "; });
+      detail.append("span")
+          .text(function(d) { return source[d] ? source[d] : "null"; });
+    });
+
+  }
+
+  //
+  //
+  //
+  function getSources(styles) {
+    var svgs = d3.selectAll("svg"),
+        info = [];
+
+    svgs.each(function() {
+      var svg = d3.select(this);
       svg.attr("xmlns", d3.ns.prefix.svg)
           .attr("version", "1.1")
         .insert("defs", ":first-child")
         .append("style")
           .attr("type", "text/css");
 
-      for (var i = 0; i < styleSheets.length; i++) {
-        processStyleSheet(styleSheets[i]);
-      }
+      var source = (new XMLSerializer()).serializeToString(svg.node()).replace('</style>', '<![CDATA[' + styles + ']]></style>');
+      var rect = svg.node().getBoundingClientRect()
+      info.push({
+        top: rect.top,
+        left: rect.left,
+        width: rect.width,
+        height: rect.height,
+        class: svg.attr("class"),
+        id: svg.attr("id"),
+        childElementCount: svg.node().childElementCount,
+        source: [doctype + source]
+      });
 
-      var svgSource = (new XMLSerializer()).serializeToString(svg.node()).replace('</style>', '<![CDATA[' + styles + ']]></style>');
-      var svgOutput = [doctype + svgSource];
-      link.attr("href", URL.createObjectURL(new Blob(svgOutput, { "type" : "text\/xml" })));
-
-      function processStyleSheet(ss) {
-        for (var i = 0; i < ss.cssRules.length; i++) {
-          var rule = ss.cssRules[i];
-          if (rule.type === 3) {
-            // Import Rule
-            processStyleSheet(rule.styleSheet);
-          } else {
-            // hack for illustrator crashing
-            // if (rule.selectorText.indexOf(">") === -1) {
-              styles += "\n" + rule.cssText;
-            // };
-          }
-        }
-      }
-    })
-    .on("mouseout", function() {
-      setTimeout(function() {
-        URL.revokeObjectURL(this.href);
-      }, 10);
     });
 
+    return info;
   }
 
-};
+  //
+  //
+  //
+  function download(source) {
+    var url = URL.createObjectURL(new Blob(source, { "type" : "text\/xml" }));
+
+    var a = d3.select("body")
+        .append('a')
+        .attr("download", "untitled.svg")
+        .attr("href", url)
+        .style("display", "none");
+
+    a.node().click();
+
+    setTimeout(function() {
+      URL.revokeObjectURL(url);
+    }, 10);
+  }
+
+  //
+  //
+  //
+  function getStyles() {
+    var styles = "",
+        styleSheets = window.document.styleSheets;
+
+    for (var i = 0; i < styleSheets.length; i++) {
+      processStyleSheet(styleSheets[i]);
+    }
+
+    function processStyleSheet(ss) {
+      for (var i = 0; i < ss.cssRules.length; i++) {
+        var rule = ss.cssRules[i];
+        if (rule.type === 3) {
+          // Import Rule
+          processStyleSheet(rule.styleSheet);
+        } else {
+          // hack for illustrator crashing
+          // if (rule.selectorText.indexOf(">") === -1) {
+            styles += "\n" + rule.cssText;
+          // };
+        }
+      }
+    }
+
+    return styles;
+  }
+
+})();
